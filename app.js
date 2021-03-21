58a1b8f072f9231b8ed3a6a764d9e5233ad7a30f
sankets25 <sankets25@gmail.com>
2021-03-21 14:13:20 -0500

    home page route

--- app.js
+++ app.js
@@ -3,5 +3,21 @@ const express = require('express')
 const app = express()
 port = 3000
 
+//serving static files in Express 
+app.use(express.static('public'))
+//__dirname is the root directory of the folder
+app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/img', express.static(__dirname + 'public/img'))
+app.use('/js', express.static(__dirname + 'public/js'))
+
+//Templating engine set to ejs
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+//Routes
+const newsRouter = require('./src/routes/news')
+
+app.use('/', newsRouter)
+
 //Listen on port 3000
 app.listen(port, () => console.log(`Listening on port ${port}`))
---
+++ src/routes/news.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const newsRouter = express.Router()
+
+newsRouter.get('', async(req, res) => {
+    res.render('news')
+})
+
+module.exports = newsRouter
