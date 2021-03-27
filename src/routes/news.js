0ccdce2498466fc5f267589241cc6a1433cf2869
sankets25 <sankets25@gmail.com>
2021-03-27 15:07:59 -0500

    API call - news added

--- src/routes/news.js
+++ src/routes/news.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const newsRouter = express.Router()
-const express = require('express')
+const axios = require('axios')
 
 newsRouter.get('', async(req, res) => {
-    res.render('news')
+    try {
+        const newsAPI = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=30cddb311c744fd38d05a6a0bfcfbe93`)
+        res.render('news', { articles : newsAPI.data.articles })
+        // console.log(newsAPI.data.articles)
+        
+    } catch (error) {
+        if(error.response){
+            res.render('news', { articles : null })
+            console.log(error.response.data)
+            console.log(error.response.status)
+            console.log(error.response.headers)
+        }else if(error.requiest){
+            res.render('news', { articles : null })
+            console.log(error.requiest)
+        } else {
+            res.render('news', { articles : null })
+            console.error('Error', error.message    )
+        }
+        
+    }
+    
 })
 
 module.exports = newsRouter
